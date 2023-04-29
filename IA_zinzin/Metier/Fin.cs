88c2d8d55f626b7c999b33f520264a018c25348e
Celeste <102237972+CelestixlMoon@users.noreply.github.com>
2023-04-29 18:49:18 +0200

    Début Fin mais aled

---
+++ IA_zinzin/Metier/Fin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IA_zinzin.Metier
+{
+    internal class Fin
+    {
+
+        public Fin() { }
+        public GagneSuicide()
+        {
+
+        }
+
+        public GagneDerniereCouche()
+        {
+            if Nain(1).pioche != 2 && NbNain > 2
+            {
+                RetirerNain(0);
+                TpMeilleure
+            }
+            else
+            {
+                RetirerNain(1);
+            }
+
+        }
+
+        //Perdre 
+        public PerdreDerniereCouche()
+        {
+            RetireTousNains();
+        }
+    }
+}
